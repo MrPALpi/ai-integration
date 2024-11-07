f76b353f22aee4b08ff63c51b93d2411cc9c85af
MrPALpi <kapralov-02@bk.ru>
2024-11-07 19:01:22 +0500

    поправил api историй

--- src/entities/history/api/index.js
+++ src/entities/history/api/index.js
@@ -1,7 +1,8 @@
 import { axiosInstance } from "@/shared/api";
+import * as toast from '@/shared/lib/toast.js';
 
 const getStories = async () => {
-  const res = await axiosInstance.post('/api/stories/', { email, password });
+  const res = await axiosInstance.post('/api/stories/');
   return res?.data;
 }
 
